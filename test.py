a1744d77f05d6ad6618b1cb886be824b93896c5c
solominh <hoangminhftu2@gmail.com>
2017-01-18 02:26:09 +0700

    Fix test

--- test.py
+++ test.py
@@ -1,12 +1,16 @@
 from stardict import load_dict
+DEBUG = True
 
 
-def test():
-    dict_dir = 'stardict-dictd_anh-viet-2.4.2'
-    try:
+def test(dict_dir):
+    if DEBUG:
         file_stream = load_dict(dict_dir)
-    except:
-        file_stream = None
+    else:
+        try:
+            file_stream = load_dict(dict_dir)
+        except Exception as e:
+            print(e.args)
+            file_stream = None
 
     if file_stream is None:
         print('Load dictionary {} failed'.format(dict_dir))
@@ -15,12 +19,12 @@ def test():
     # Test dict index
     dict_reader = file_stream['dict']
     for i in range(10):
-        print(dict_reader.get_dict_by_index(i)[
-              'm'].decode('utf-8', errors='ignore'))
+        for k, v in dict_reader.get_dict_by_index(i).items():
+            print(v.decode('utf-8', errors='ignore'))
 
     # Test dict word
     ifo_reader = file_stream['ifo']
-    definitions = dict_reader.get_dict_by_word(r"'cellist")
+    definitions = dict_reader.get_dict_by_word(r"hello")
     for definition in definitions:
         sametypesequence = ifo_reader.get_ifo('sametypesequence')
         if sametypesequence:
@@ -31,5 +35,6 @@ def test():
     # read_idx_file(dict_files['idx'])
 
 
-
-test()
+# dict_dir = 'stardict-dictd_anh-viet-2.4.2'
+dict_dir = 'stardict-Collins_Cobuild_5-2.4.2'
+test(dict_dir)
