44d985d991a11eab645c0d2fb44bbee2c5c4423d
solominh <hoangminhftu2@gmail.com>
2017-01-18 16:50:47 +0700

    Clean up

--- test.py
+++ test.py
@@ -23,27 +23,6 @@ def test(dict_dir):
     for k, v in definition.items():
         print(v.decode('utf-8', errors='ignore'))
 
-    # Test dict word
-    test_word = "hi"
-    definitions = dict_reader.get_dict_by_word(test_word)
-    for definition in definitions:
-        for k, v in definition.items():
-            print(v.decode('utf-8', errors='ignore'))
-
-    # Print synonym
-    syn_reader = file_stream['syn']
-    if syn_reader:
-        indexes = syn_reader.get_syn(test_word)
-        idx_reader = file_stream['idx']
-        for index in indexes:
-            word_str, word_data_offset, word_data_size = idx_reader.get_index_by_num(
-                index)
-            print(word_str + ', ', end='')
-        print('')
-
-    # read_ifo_file(dict_files['ifo'])
-    # read_idx_file(dict_files['idx'])
-
 
 def test2(dict_dir):
     file_stream = load_dict(dict_dir)
