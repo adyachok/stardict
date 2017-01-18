da0dc2918c4e52f8ad483e61fa63b283a16340aa
solominh <hoangminhftu2@gmail.com>
2017-01-19 01:15:45 +0700

    Print more

--- test.py
+++ test.py
@@ -1,16 +1,16 @@
 from stardict import Stardict
 
 
-def test():
+def test(word_str):
     dictionaries_dirpath = './'
     stardict = Stardict(dictionaries_dirpath)
-    word_str = 'hello'
     all_dictionaries_definitions = stardict.get_definitions_from_all_dictionaries(
         word_str)
-    for dictnary_name, definitions in all_dictionaries_definitions.items():
+    for dictionary_name, definitions in all_dictionaries_definitions.items():
+        print(dictionary_name)
         for definition in definitions:
             for k, v in definition.items():
                 print(v.decode('utf-8', errors='ignore'))
 
 
-test()
+test('hello')
